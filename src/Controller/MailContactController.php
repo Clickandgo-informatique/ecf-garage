060dff2cab0c1a0feae1b7febfc8b050a57cea0d
Jean Sérien <clavinova1@free.fr>
2023-06-17 23:56:57 +0200

    Suite mise en place mailing Services

--- src/Controller/MailContactController.php
+++ src/Controller/MailContactController.php
@@ -4,36 +4,46 @@ namespace App\Controller;
 
 use App\Form\ContactFormType;
 use App\Repository\ServicesRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/admin/contact-par-mail', name: 'app_contact_par_mail_')]
 class MailContactController extends AbstractController
 {
     #[Route('/envoyer-mail/{id}/', name: 'envoyer_mail')]
-    public function envoyerMail(Request $request, ServicesRepository $servicesRepository,$id): Response
+    public function envoyerMail(MailerInterface $mailer,Request $request, ServicesRepository $servicesRepository,$id): Response
     {
         //Si l'on se trouve sur la page ou fiche d'un service
         $service_a_contacter = $servicesRepository->find($id);
-        dd($service_a_contacter);
+ 
         //Recherche des adresses mails du service
-        $mail1 = $service_a_contacter->getMailService1();
-        $mail2 = $service_a_contacter->getMailService2();
-        dd($mail1);
+        $mail1 = $service_a_contacter->getMailService1(); 
 
         $form = $this->createForm(ContactFormType::class);
-        $form->handleRequest($request);
+        $contact=$form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $email=(new TemplatedEmail())
+            ->from($contact->get('email')->getData())
+            ->to($mail1)
+            ->subject($contact->get('title')->getData())
+            ->htmlTemplate('emails/contact_service.html.twig')
+            ->context([
+                'service'=>$service_a_contacter,
+                'mail'=>$mail1,
+                'message'=>$contact->get('message')->getData()
+            ]);
 
             $this->addFlash('message', 'Le mail a bien été envoyé au service demandé, nous vous répondrons dans les meilleurs délais.');
         }
 
         return $this->render('_modale-contact.html.twig', [
-            'mail1' => $mail1,
-            'mail2' => $mail2,            
+            'mail1' => $mail1,                        
             'service' => $service_a_contacter,
             'form' => $form->createView()
         ]);
--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -10,27 +10,35 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotBlankValidator;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class ContactFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('service',EntityType::class,[
-            'class'=>Services::class,
-            'label'=>'Service contacté :'
-        ])
-            ->add('titre', TextType::class, [
+            ->add('service', EntityType::class, [
+                'class' => Services::class,
+                'label' => 'Service contacté :',
+            ])
+            ->add('subject', TextType::class, [
+                'label' => "Objet :",
                 'disabled' => true,
-                'attr' => ['class' => 'form-control']
+                'attr' => ['class' => 'form-control'],
+                'constraints' => new NotBlank(), new NotNull()
+
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Votre e-mail',
-                'attr' => ['class' => 'form-control']
+                'attr' => ['class' => 'form-control'],
+                'constraints' => new NotBlank(), new NotNull()
             ])
             ->add('message', CKEditorType::class, [
-                'label' => 'Votre message'
-            ]);           
+                'label' => 'Votre message',
+                'constraints' => new NotBlank(), new NotNull()
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
