a495a060c66f1f7eaa4bae5bcd8113ed63af6062
Jean Sérien <clavinova1@free.fr>
2023-06-19 15:40:19 +0200

    mise en place systeme switch + ajax pour publication des annonces

--- public/assets/js/annonces.js
+++ public/assets/js/annonces.js
@@ -1,12 +1,17 @@
 window.onload = () => {
+  console.log("coucou1");
   //Active ou désactive la publication d'une fiche véhicule en tant qu'annonce
-    let publier = document.querySelectorAll(".publication-annonce");
-  
-    for (let checkbox of publier) {
-      checkbox.addEventListener("click", function () {
-        let xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("get",`/vehicules/annonces/publier-annonce/${this.dataset.id}`)
-        xmlhttp.send()
-      });
-    }
-  };
+  let publier = document.querySelectorAll(".toggle-switch-publier");
+
+  for (let checkbox of publier) {
+    checkbox.addEventListener("click", function () {
+      let xmlhttp = new XMLHttpRequest();
+
+      xmlhttp.open(
+        "get",
+        `/vehicules/annonces/publier-annonce/${this.dataset.id}`
+      );
+      xmlhttp.send();
+    });
+  }
+};
