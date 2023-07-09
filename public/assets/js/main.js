105b92c5a7527d1495eeb90ccafc7a09b23572df
Jean Sérien <clavinova1@free.fr>
2023-07-10 01:17:21 +0200

    Améliorations navbar responsive + hamburger

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,17 +2,17 @@ window.onload = () => {
   //Fonction qui anime les alertes de type flash
   const btnClose = document.querySelectorAll(".btn-close").forEach((button) => {
     button.addEventListener("click", (e) => {
-      e.target.parentElement.style.webkitTransform = 'scale(0)';
-      e.target.parentElement.style.msTransform = 'scale(0)';
-      e.target.parentElement.style.mozTransform = 'scale(0)';
-      e.target.parentElement.style.oTransform = 'scale(0)';
-      e.target.parentElement.style.transform = 'scale(0)';
+      e.target.parentElement.style.webkitTransform = "scale(0)";
+      e.target.parentElement.style.msTransform = "scale(0)";
+      e.target.parentElement.style.mozTransform = "scale(0)";
+      e.target.parentElement.style.oTransform = "scale(0)";
+      e.target.parentElement.style.transform = "scale(0)";
 
-      e.target.parentElement.style.webkitTransition = 'transform 1s';
-      e.target.parentElement.style.MozTransition = 'transform 1s';
-      e.target.parentElement.style.msTransition = 'transform 1s';
-      e.target.parentElement.style.oTransition = 'transform 1s';
-      e.target.parentElement.style.transition = 'transform 1s';
+      e.target.parentElement.style.webkitTransition = "transform 1s";
+      e.target.parentElement.style.MozTransition = "transform 1s";
+      e.target.parentElement.style.msTransition = "transform 1s";
+      e.target.parentElement.style.oTransition = "transform 1s";
+      e.target.parentElement.style.transition = "transform 1s";
 
       setTimeout(() => {
         e.target.parentElement.remove();
@@ -21,10 +21,17 @@ window.onload = () => {
   });
 
   //Fonction pour navbar responsive
-  const menuHamburger=document.querySelector('.menu-hamburger')
-  const menuNavbar=document.querySelector('.menu-navbar')
+  const menuHamburger = document.querySelector(".menu-hamburger");
+  const menuNavbar = document.querySelector(".menu-navbar");
+  const toggleNav = (e) => {
+    menuHamburger.classList.toggle("open");
+    const ariaToggle =
+      menuHamburger.getAttribute("aria-expanded") === "true" ? "false" : "true";
+    menuHamburger.setAttribute("aria-expanded", ariaToggle);
+    menuNavbar.classList.toggle("open");
+  };
 
-  menuHamburger.addEventListener('click',()=>{   
-    menuNavbar.classList.toggle('mobile-menu')
-  })
+  menuHamburger.addEventListener("click", () => {
+    toggleNav();
+  });
 };
