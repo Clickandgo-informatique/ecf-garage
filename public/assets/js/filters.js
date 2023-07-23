d38ce755d52a74e122a4644b9589d1946aefb5f1
Jean Sérien <clavinova1@free.fr>
2023-07-24 00:33:36 +0200

    Phase 1 responsive filtres

--- public/assets/js/filters.js
+++ public/assets/js/filters.js
@@ -3,48 +3,51 @@ window.onload = () => {
   const filtersForm = document.querySelector("#filters");
 
   document.querySelectorAll("#filters input").forEach((input) => {
-    input.addEventListener("change", () => {
-      // if(input.type =="number"){
+    if (input.className != "btn-collapse") {
+      input.addEventListener("change", () => {
+        if (input.value !== "" && input.value !== null) {
+          //Récupération des données du formulaire
+          const Form = new FormData(filtersForm);
 
-      // }
-      if (input.value !== "" && input.value !== null) {
-        //Récupération des données du formulaire
-        const Form = new FormData(filtersForm);
+          //Création de la queryString
+          const Params = new URLSearchParams();
 
-        //Création de la queryString
-        const Params = new URLSearchParams();
+          Form.forEach((value, key) => {
+            Params.append(key, value);
+          });
 
-        Form.forEach((value, key) => {
-          Params.append(key, value);
-        });
+          //Récupération de l'url active
+          const Url = new URL(window.location.href);
 
-        //Récupération de l'url active
-        const Url = new URL(window.location.href);
+          //Lancement requête Ajax
 
-        //Lancement requête Ajax
-
-        fetch(Url.pathname + "?" + Params.toString() + "&ajax=1", {
-          headers: {
-            "X-Requested-With": "XMLHttpRequest",
-          },
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            //Recherche de la zone de contenu
-            const content = document.querySelector("#content");
-
-            //Remplacement du contenu
-            content.innerHTML = data.content;
-
-            //Mise à jour de l'url
-            history.pushState({}, null, Url.pathname + "?" + Params.toString());
+          fetch(Url.pathname + "?" + Params.toString() + "&ajax=1", {
+            headers: {
+              "X-Requested-With": "XMLHttpRequest",
+            },
           })
-          .catch((e) => alert(e));
-      } else {
-        alert("Le champ de filtre ne peut pas être laissé vide !");
-        // input.value = 0;
-      }
-    });
+            .then((response) => response.json())
+            .then((data) => {
+              //Recherche de la zone de contenu
+              const content = document.querySelector("#content");
+
+              //Remplacement du contenu
+              content.innerHTML = data.content;
+
+              //Mise à jour de l'url
+              history.pushState(
+                {},
+                null,
+                Url.pathname + "?" + Params.toString()
+              );
+            })
+            .catch((e) => alert(e));
+        } else {
+          alert("Le champ de filtre ne peut pas être laissé vide !");
+          // input.value = 0;
+        }
+      });
+    }
   });
 
   //Affiche le menu de filtres lateral gauche
@@ -71,6 +74,8 @@ window.onload = () => {
       }, delay);
     };
   }
+  //Collapse les containers d'input
+  
 
   //Contrôle des saisies
   const yearMin = document.querySelector("#yearMin");
